9fa962091f89c669df021104133ba1871ec316b0
Mohamed Belkamel <belkamelmohamed@gmail.com>
2023-11-01 17:33:18 +0100

    docs: added demo video

--- main.go
+++ main.go
@@ -78,20 +78,13 @@ func main() {
 
 func maskPublicIPs(input string) string {
 	ipv4Regex := `\b(?:\d{1,3}\.){3}\d{1,3}\b`
-	ipv6Regex := `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`
 	re := regexp.MustCompile(ipv4Regex)
-	re6 := regexp.MustCompile(ipv6Regex)
 	matches := re.FindAllString(input, -1)
-	ipv6Matches := re6.FindAllString(input, -1)
 
 	for _, match := range matches {
 		if isPublicIPv4(match) {
 			input = strings.Replace(input, match, customMask, -1)
 		}
-	}
-
-	for _, match := range ipv6Matches {
-		input = strings.Replace(input, match, customMask, -1)
 	}
 
 	if maskDNS {
